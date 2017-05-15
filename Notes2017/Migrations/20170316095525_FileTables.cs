2e7dff142c99eb42f438603ed004f2b8b3bf9306
drsinder <sinder@illinois.edu>
2017-05-15 12:19:19 -0500

    Add files via upload

---
+++ Notes2017/Migrations/20170316095525_FileTables.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Notes2017.Migrations
+{
+    public partial class FileTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SQLFile",
+                columns: table => new
+                {
+                    FileId = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Comments = table.Column<string>(maxLength: 1000, nullable: true),
+                    ContentID = table.Column<long>(nullable: false),
+                    ContentType = table.Column<string>(maxLength: 100, nullable: false),
+                    Contributor = table.Column<string>(maxLength: 300, nullable: false),
+                    FileName = table.Column<string>(maxLength: 300, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SQLFile", x => x.FileId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SQLFileContent",
+                columns: table => new
+                {
+                    ContentId = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Content = table.Column<byte[]>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SQLFileContent", x => x.ContentId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SQLFile_FileName",
+                table: "SQLFile",
+                column: "FileName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SQLFile");
+
+            migrationBuilder.DropTable(
+                name: "SQLFileContent");
+        }
+    }
+}
---
+++ Notes2017/Migrations/SQLFileDbContextModelSnapshot.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Notes2017.Data;
+
+namespace Notes2017.Migrations
+{
+    [DbContext(typeof(SQLFileDbContext))]
+    partial class SQLFileDbContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("Notes2017.Models.SQLFile", b =>
+                {
+                    b.Property<long>("FileId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Comments")
+                        .HasMaxLength(1000);
+
+                    b.Property<long>("ContentID");
+
+                    b.Property<string>("ContentType")
+                        .IsRequired()
+                        .HasMaxLength(100);
+
+                    b.Property<string>("Contributor")
+                        .IsRequired()
+                        .HasMaxLength(300);
+
+                    b.Property<string>("FileName")
+                        .IsRequired()
+                        .HasMaxLength(300);
+
+                    b.HasKey("FileId");
+
+                    b.HasIndex("FileName");
+
+                    b.ToTable("SQLFile");
+                });
+
+            modelBuilder.Entity("Notes2017.Models.SQLFileContent", b =>
+                {
+                    b.Property<long>("ContentId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<byte[]>("Content")
+                        .IsRequired();
+
+                    b.HasKey("ContentId");
+
+                    b.ToTable("SQLFileContent");
+                });
+        }
+    }
+}
